8a95435e78b81f91512742a1c94e6046e2026654
jaborl <paleodreilar@outlook.es>
2023-08-31 12:04:56 -0300

    actuazalido clase 31/08

--- 2.R
+++ 2.R
@@ -15,3 +15,43 @@ pabellon=readline("indique en que pabellon cursa: Pabellon 0, Pabellon 1 o Pabel
 cantidad<-nchar(pabellon)
 numeropabellon=substr(pabellon,cantiad,cantidad)
 turno=readline("Indique el turno de la cursada: manana, tarde o noche")
+
+#Practica 2
+x<-as.numeric(readline("ingresar un valor para calcular su raiz"))
+raiz_cuadrada<-sqrt(x)
+if(x>=0){
+  print(raiz_cuadrada)
+}
+
+x<-1
+y<-2
+if((x<-0 & y>0)){
+  print("ambos numeros son positivos")
+}
+
+
+nota<-as.numeric(readline("ingresar nota"))
+if(0<=nota & nota<=10){
+  
+}
+  if(nota>=4){
+    print("examen aprobado")
+  }else{
+    print("examen desaprobado")
+  }
+
+
+Tmax<-readline("ingresar valor de temperatura maxima registrada :")
+Tmax<-as.numeric(Tmax)
+Tmin<-readline("ingresar valor de temperatura minina registrada :")
+Tmin<-as.numeric(Tmin)
+if(Tmax<Tmin){
+  print("Tmax menor a Tmin, verificar los datos")
+}else if(Tmax==Tmin){
+  print("Tmax igual a Tmin, verificar datos")
+}else {
+  amplitud_termica<-Tmax-Tmin
+  print(paste("La amplitud diaria fue de",amplitud_termica))
+}
+  
+
