a0f4bc5ca815351cc37f440b95c2373e12716032
colomboromina <142675480+colomboromina@users.noreply.github.com>
2023-09-04 23:55:37 -0300

    Update entrega_practica1_codigo.R

--- entrega_practica1_codigo.R
+++ entrega_practica1_codigo.R
@@ -1,9 +1,9 @@
-Nombre<-readline("Ingresar nombre :")
-Apellido<-readline("ingresar apellido :")
+nombre<-readline("Ingresar nombre :")
+apellido<-readline("ingresar apellido :")
 libreta_universitaria<-readline("Ingresar libreta universitaria en formato N/AA :")
 cantidad_caracteres<-nchar(Libreta_universitaria)
 numero_libreta<-substr(Libreta_universitaria,1,Cantidad-3)
 ano_inscripcion<-substr(Libreta_universitaria,5,Cantidad)
-Materias<-as.numeric(readline("Ingresar cantidad de materias aprobadas :"))
+materias<-as.numeric(readline("Ingresar cantidad de materias aprobadas :"))
 materias_restastes<-20-Materias
 print(paste("El alumno",Nombre,Apellido,"se inscribio como alumno de Exactas en el puesto",numero_libreta,paste("en el ano 20",ano_inscripcion,sep = ""),"y debe aprobar",materias_restastes,"materias para obtener el titulo de grado" ))
