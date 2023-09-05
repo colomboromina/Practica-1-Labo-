fce3ba5c5d7b5561bfb5d922f1156356c2857a62
colomboromina <romi.colombo23@gmail.com>
2023-09-04 23:54:13 -0300

    entrega tp1

--- entrega_practica1_codigo.R
+++ entrega_practica1_codigo.R
@@ -1,9 +1,9 @@
 Nombre<-readline("Ingresar nombre :")
 Apellido<-readline("ingresar apellido :")
-Libreta_universitaria<-readline("Ingresar libreta universitaria en formato N/AA :")
-Cantidad<-nchar(Libreta_universitaria)
+libreta_universitaria<-readline("Ingresar libreta universitaria en formato N/AA :")
+cantidad_caracteres<-nchar(Libreta_universitaria)
 numero_libreta<-substr(Libreta_universitaria,1,Cantidad-3)
 ano_inscripcion<-substr(Libreta_universitaria,5,Cantidad)
 Materias<-as.numeric(readline("Ingresar cantidad de materias aprobadas :"))
-x<-20-Materias
-print(paste("El alumno",Nombre,Apellido,"se inscribio como alumno de Exactas en el puesto",numero_libreta,paste("en el ano 20",ano_inscripcion,sep = ""),"y debe aprobar",x,"materias para obtener el titulo de grado" ))
+materias_restastes<-20-Materias
+print(paste("El alumno",Nombre,Apellido,"se inscribio como alumno de Exactas en el puesto",numero_libreta,paste("en el ano 20",ano_inscripcion,sep = ""),"y debe aprobar",materias_restastes,"materias para obtener el titulo de grado" ))
